922ce0b39aefca8c241ee2e012056dd07bf76f43
Bartek Bugała <bartek.bugala@gmail.com>
2019-05-14 23:26:27 +0200

    modified:   css/style.css 	modified:   css/style.css.map 	modified:   index.html 	modified:   js/script.js 	modified:   sass/_flickity-settings.scss 	modified:   sass/_layout.scss 	modified:   sass/_mixins.scss

--- js/script.js
+++ js/script.js
@@ -6,10 +6,32 @@ var flkty = new Flickity( elem, {
   hash: true,
 });
 
+var progressBar = document.querySelector('.progress-bar');
+var btnStart = document.querySelector('.btn');
 
+// vanilla JS
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+////////////////////
+
+
+
+btnStart.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.btn' ) ) {
+    return;
+  }
+  var selector = event.target.getAttribute('data-selector');
+  flkty.selectCell( selector );
+});
+
+
