7c978b59982d5d66762fdae6ccd30adef967e317
Bartek Bugała <bartek.bugala@gmail.com>
2019-05-16 19:15:28 +0200

    modified:   css/style.css 	modified:   css/style.css.map 	modified:   index.html 	new file:   js/mustache.js 	modified:   js/script.js 	modified:   sass/_mixins.scss

--- js/script.js
+++ js/script.js
@@ -33,20 +33,3 @@ btnStart.addEventListener( 'click', function( event ) {
 });
 
 
-/////// MUSTACHE
-(function(){ 
-var carouselItem = document.getElementById('carousel-item').innerHTML;
-
-Mustache.parse(carouselItem);
-
-////
-
-var listItems = '';
-
-for(var i = 0; i < mySlides.length; i++){
-  listItems += Mustache.render(carouselItem, mySlides[i]);
-};
-
-results.insertAdjacentHTML('beforeend', listItems);
-
-})(); 
